0a6d916b7cf8e0de6dc3d6a5adaff853797f6a73
samuel Odubamowo <wemsamuel@gmail.com>
2021-05-20 15:28:32 -0700

    updated front end

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json())
+app.use(express.json());
+app.use(express.static('public'));
 const { animals } = require('./data/animals.json');
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -93,6 +94,21 @@ app.post('/api/animals', (req, res) => {
     }
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
